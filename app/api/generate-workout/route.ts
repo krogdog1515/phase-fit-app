ff670cbc653acb248adec5d678b9126212def10d
Kyle Rogers <kylerogers@MacBook-Pro-6.local>
2026-04-22 17:38:45 -0700

    fix api supabaseurl bug

--- app/api/generate-workout/route.ts
+++ app/api/generate-workout/route.ts
@@ -8,6 +8,8 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY!,
 });
 
+console.log("API SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
